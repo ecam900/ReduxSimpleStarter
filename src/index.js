35380fa6e73de959a053005b2c2fca82f7ac9cf5
Eric Crescioni <ecam900@gmail.com>
2018-02-11 17:07:48 -0400

    Import React to index.js. Make App constant function Call render on React to see what error outputs.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Create new component. This component should produce some HTML.
+const App = function() {
+    return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM).
+
+React.render(App);
